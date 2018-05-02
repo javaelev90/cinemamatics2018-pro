f1e5932c5bcdf6d04d705a4ca16ad65ca0daefd0
elev <elev@172.20.0.216>
2018-05-02 13:08:41 +0200

    Created new objects in main

--- CinemaMatics2018-pro/src/Program.java
+++ CinemaMatics2018-pro/src/Program.java
@@ -5,8 +5,21 @@ public class Program {
 		// TODO Auto-generated method stub
 		
 		UserInterface ui = new UserInterface();
+		Cinema myCinema = new Cinema("Sandrews");
+		Theatre s1 = new Theatre("Salong1");
+		Theatre s2 = new Theatre("Salong2");
+		Theatre s3 = new Theatre("Salong3");
+		Theatre s4 = new Theatre("Salong4");
+		
+		
+		myCinema.addTheatre(s1);
+		myCinema.addTheatre(s2);
+		myCinema.addTheatre(s3);
+		myCinema.addTheatre(s4);
+		
 		
 		while(true) {
+			
 			
 			ui.show_menu();
 			Integer choice = UserInterface.inputInt();
