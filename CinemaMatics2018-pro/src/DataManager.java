db139e0cce82a485fa762b6fe100ee28dbcccd35
javaelev90 <37899614+javaelev90@users.noreply.github.com>
2018-05-02 12:01:59 +0200

    Made initial implementations of Datamanager, Seat and Movie classes

--- CinemaMatics2018-pro/src/DataManager.java
+++ CinemaMatics2018-pro/src/DataManager.java
@@ -1,4 +1,65 @@
 
-public class DataManager {
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
+public class DataManager {
+	
+	private Map<Integer, Show> shows;
+	
+	public DataManager() {
+		shows = new HashMap<Integer, Show>();
+	}
+	
+	/**
+	 * 
+	 * @param id for the show
+	 * @return the Show
+	 */
+	public Show getShow(Integer id) {
+		return shows.get(id);
+	}
+	
+	/**
+	 * 
+	 * @return all shows
+	 */
+	public List<Show> getShows(){
+		return shows.values().stream().collect(Collectors.toList());
+	}
+	
+	/**
+	 * 
+	 * @param show
+	 * @param id for the show
+	 * @return check if the show already exists
+	 */
+	public boolean createShow(Show show, Integer id) {
+		if(!shows.containsKey(id)) {
+			shows.put(id, show);
+			return true;
+		}
+		return false;
+	}
+	
+	/**
+	 * 
+	 * @param booking
+	 * @param showId
+	 * @return if the booking could be made
+	 */
+	public boolean createBooking(Booking booking, Integer showId) {
+		return false;
+	}
+	
+	/**
+	 * 
+	 * @param bookingId
+	 * @return if booking was found
+	 */
+	public boolean getBooking(Integer bookingId) {
+		return false;
+	}
+	
 }
--- CinemaMatics2018-pro/src/Seat.java
+++ CinemaMatics2018-pro/src/Seat.java
@@ -1,4 +1,18 @@
 
 public class Seat {
 
+	private Integer bookingId;
+
+	public Integer getBookingNr() {
+		return bookingId;
+	}
+
+	public void setBookingNr(Integer bookingId) {
+		this.bookingId = bookingId;
+	}
+	
+	public boolean isOccupied() {
+		return (bookingId == null) ? false : true;
+	}
+	
 }
