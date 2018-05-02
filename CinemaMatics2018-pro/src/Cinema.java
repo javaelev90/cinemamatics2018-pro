8d2b601e41f1eeb900497df0f08087458ca3a892
elev <elev@172.20.0.216>
2018-05-02 11:53:39 +0200

    Made setters and getters for booking, bookingHandler, cinema and customer

--- CinemaMatics2018-pro/src/Cinema.java
+++ CinemaMatics2018-pro/src/Cinema.java
@@ -1,4 +1,22 @@
+import java.util.ArrayList;
+import java.util.List;
 
 public class Cinema {
-
+	
+	private String cinemaName;
+	List<Theatre> myTheatres;
+	
+	public Cinema(String cName) {
+		cinemaName = cName;
+		myTheatres = new ArrayList<>();
+	}
+	
+	public String getCinemaName() {
+		return cinemaName;
+	}
+	
+	public void addTheatre(Theatre t) {
+		myTheatres.add(t);
+	}
+	
 }
--- CinemaMatics2018-pro/src/Customer.java
+++ CinemaMatics2018-pro/src/Customer.java
@@ -1,4 +1,28 @@
 
 public class Customer {
-
+	private int custId;
+	private String name;
+	private int age;
+	
+	public void setCustId(int id) {
+		custId = id;
+	}
+	public int getCustId() {
+		return custId;
+	}
+	
+	public void setName(String n) {
+		name = n;
+	}
+	public String getName() {
+		return name;
+	}
+	
+	public void setAge(int a) {
+		age = a;
+	}
+	public int getAge() {
+		return age;
+	}
+	
 }
