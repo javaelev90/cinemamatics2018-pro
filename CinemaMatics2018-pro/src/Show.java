22d4f53542312fb30e5f8b7a65a04c1ee8c8a9b7
JH <johan.haggqvist@gmail.com>
2018-05-02 11:26:47 +0200

    Show Class, setters and getters

--- CinemaMatics2018-pro/src/Show.java
+++ CinemaMatics2018-pro/src/Show.java
@@ -1,4 +1,47 @@
+import java.time.LocalDateTime;
+
+// Describes a show in the theatre, Start, End and Movie
 
 public class Show {
+	private LocalDateTime start;
+	private LocalDateTime end;
+	private Movie movie;
+	
+	/**
+	 * @return the start
+	 */
+	public LocalDateTime getStart() {
+		return start;
+	}
+	/**
+	 * @param start the start to set
+	 */
+	public void setStart(LocalDateTime start) {
+		this.start = start;
+	}
+	/**
+	 * @return the end
+	 */
+	public LocalDateTime getEnd() {
+		return end;
+	}
+	/**
+	 * @param end the end to set
+	 */
+	public void setEnd(LocalDateTime end) {
+		this.end = end;
+	}
+	/**
+	 * @return the movie
+	 */
+	public Movie getMovie() {
+		return movie;
+	}
+	/**
+	 * @param movie the movie to set
+	 */
+	public void setMovie(Movie movie) {
+		this.movie = movie;
+	}
 
 }
