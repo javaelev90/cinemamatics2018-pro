d4ab16483e913a643d3c5d0327be3c43cb78c0b9
JH <johan.haggqvist@gmail.com>
2018-05-02 11:48:12 +0200

    Some very basic Crap for the UserInterface

--- CinemaMatics2018-pro/src/UserInterface.java
+++ CinemaMatics2018-pro/src/UserInterface.java
@@ -1,10 +1,28 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class UserInterface {
+	
+	static BufferedReader con = new BufferedReader(new InputStreamReader(System.in));
 	
 	public void show_menu() {
 		System.out.println("-----------------------------------");
 		System.out.println("-Menu Here						  -");
 		System.out.println("-----------------------------------");
+	}
+	
+	// Get Int from console
+	public static int inputInt() {
+		int myInt = 0;
+		try {
+			myInt = Integer.parseInt(con.readLine());
+		} catch (NumberFormatException | IOException e) {
+			System.out.println("Bad input");
+			myInt = inputInt();
+		}
+
+		return myInt;
 	}
 
 }
